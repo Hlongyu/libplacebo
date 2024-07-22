97cc98e92c733ef64438f706c66e58fc1c35836f
hly <741114316@qq.com>
2024-07-22 20:15:12 +0800

    1

--- src/common.h
+++ src/common.h
@@ -43,7 +43,8 @@
 #define PL_PRINTF(fmt, va) __attribute__ ((format(printf, fmt, va))) \
                            __attribute__ ((nonnull(fmt)))
 #elif defined(_MSC_VER)
-#define PL_PRINTF(fmt, va) __declspec(format_string(fmt))
+#define PL_PRINTF(fmt, va) __declspec(format("printf", fmt, va)) \
+                           __declspec(nonnull(fmt))
 #else
 #define PL_PRINTF(fmt, va) 
 #endif
